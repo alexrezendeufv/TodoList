8196c99b956f25761cb16ae53ce658f1360b43aa
Alexrezendeufv <alexrezendeufv@gmail.com>
2023-09-13 13:55:17 -0300

    Upando projeto finalizado no apache

---
+++ src/main/java/controller/TaskController.java
@@ -0,0 +1,158 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Date;
+import java.util.List;
+import model.Task;
+import util.ConnectionFactory;
+
+/**
+ *
+ * @author alexu
+ */
+public class TaskController {
+    
+    
+    public void Save(Task task){
+        
+        String sql = "INSERT INTO task (idProject,"
+                + " name,"
+                + " description,"
+                + "completed,"
+                + "notes,"
+                + "deadline,"
+                + "createAt,"
+                + "updateAt) VALUES (?,?,?,?,?,?,?,?)";
+        Connection c= null;
+        PreparedStatement statement = null;
+        
+        try{
+            c = ConnectionFactory.getConnection();
+            statement = c.prepareStatement(sql);
+            statement.setInt(1, task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3,task.getDescription());
+            statement.setBoolean(4, task.isIsCompleted());
+            statement.setString(5, task.getNotes());
+            statement.setDate(6, new Date(task.getDeadline().getTime()));
+            statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
+            statement.execute();
+        }catch(Exception ex){
+            throw new RuntimeException( "Erro ao salvar tarefa"+ ex.getMessage(), ex);
+        }finally{
+            ConnectionFactory.closeConnection(c,statement);
+        }
+    }
+    
+    public void updated(Task task){
+        
+        String sql = "UPDATE task SET idProject=?, "
+                + "name=?, "
+                + "description=?,"
+                + "notes=?,"
+                + "completed =?,"
+                + "deadine=?,"
+                + "createAt=?"
+                + "updateAt=?"
+                + "WHERE id=?";
+        
+        
+        Connection connection =null;
+        PreparedStatement statement= null;
+        
+        try{
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1,task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3,task.getDescription());
+            statement.setString(4,task.getNotes());
+            statement.setBoolean(5,task.isIsCompleted());
+            statement.setDate(6, (java.sql.Date) new Date(task.getDeadline().getTime()));
+            statement.setDate(7, (java.sql.Date) new Date(task.getCreatedAt().getTime()));
+            statement.setDate(8, (java.sql.Date) new Date(task.getUpdatedAt().getTime()));
+            statement.setInt(9,task.getId());
+            statement.execute();    
+        }catch(Exception e){
+            throw new RuntimeException("Deu erro aqui", e);
+        }finally{
+            ConnectionFactory.closeConnection(connection);
+        }
+        
+        
+    }
+    
+    public void removebyId(int taskId){
+        
+        String sql = "DELETE FROM task WHERE id =?";
+        Connection conn = null;
+        PreparedStatement statement =null;
+        
+        try{
+        
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareCall(sql);
+            statement.setInt(1, taskId);
+            statement.execute();
+            
+        }catch(Exception e){
+            throw new RuntimeException("Ocorreu um erro na remoção", e);
+        }finally{
+            ConnectionFactory.closeConnection(conn,statement);
+        }
+        
+    }
+    
+    public List<Task> getAll(int idProject){ //Vai retornar uma lista de tarefas se baseando no id do projeto
+        
+        String sql = "SELECT * FROM  task WHERE idProject =?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;  //Resposta no banco de dados para trazer algo de lá
+        
+        List<Task> tasks = new ArrayList<>();
+        
+        try{
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, idProject);
+            resultSet = statement.executeQuery();
+            
+            while(resultSet.next()){
+                
+                Task task = new Task();
+                task.setId(resultSet.getInt("id"));
+                task.setIdProject(resultSet.getInt("idProject"));
+                task.setName(resultSet.getString("name"));
+                task.setDescription(resultSet.getString("idProject"));
+                task.setNotes(resultSet.getString("notes"));
+                task.setIsCompleted(resultSet.getBoolean("completed"));
+                task.setDeadline(resultSet.getDate("deadline"));
+                task.setCreatedAt(resultSet.getDate("createAt"));
+                task.setUpdatedAt(resultSet.getDate("updateAt"));
+                
+                tasks.add(task);
+            }
+            
+        }catch(Exception e){
+             throw new RuntimeException("Erro em chamar as tarefas");
+        }finally{
+            ConnectionFactory.closeConnection(connection, statement, resultSet);
+        }
+       
+        //Lista de tarefas carregada do banco de dados
+        return tasks;
+    }
+
+    
+}
---
+++ src/main/java/model/Task.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author alexu
+ */
+public class Task {
+    
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private String notes;
+    private boolean isCompleted;
+    private Date deadline;
+    private Date createAt;
+    private Date updateAt;
+
+    public Task(int id, int idProject, String name, String description, String notes, boolean isCompleted, Date deadline, Date createAt, Date updateAt) {
+        this.id = id;
+        this.idProject = idProject;
+        this.name = name;
+        this.description = description;
+        this.notes = notes;
+        this.isCompleted = isCompleted;
+        this.deadline = deadline;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+    
+    public Task(){
+        this.createAt = new Date();
+        this.updateAt = new Date();
+        this.deadline = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Date getCreatedAt() {
+        return createAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updateAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updateAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + "id=" + id + ", idProject=" + idProject + ", name=" + name + ", description=" + description + ", notes=" + notes + ", isCompleted=" + isCompleted + ", deadline=" + deadline + ", createAt=" + createAt + ", updateAt=" + updateAt + '}';
+    }
+    
+    
+    
+    
+    
+    
+}
